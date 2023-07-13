8709c0750ff63840ba9168b2db8f8ce89f6a7c03
Maciej Buszkiewicz <maciek.buszkiewicz@ideeconcept.pl>
2023-07-13 03:02:21 +0200

    feat: added table component

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,12 +1,20 @@
 import React from 'react'
 
 import Hero from "../components/Hero/Hero";
+import Card from '../components/Card/Card';
+import WaterworksTable from '../components/WaterworksTable/WaterworksTable';
 
 const TablePage = () => {
   return (
     <>
       <Hero />
-      <div>TablePage</div>
+      <div className="container">
+        <div className="row">
+          <Card className="col-12">
+            <WaterworksTable />
+          </Card>
+        </div>
+      </div>
     </>
   )
 }
