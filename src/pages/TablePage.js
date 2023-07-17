19cda869399a7c5dc3f60c0b14e16382cbc4be72
Maciej Buszkiewicz <maciek.buszkiewicz@ideeconcept.pl>
2023-07-17 19:26:11 +0200

    feat: removed unused component

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,21 +1,17 @@
 import React from 'react'
 
-import Hero from "../components/Hero/Hero";
 import Card from '../components/Card/Card';
 import WaterworksTable from '../components/WaterworksTable/WaterworksTable';
 
 const TablePage = () => {
   return (
-    <>
-      <Hero />
-      <div className="container">
-        <div className="row">
-          <Card className="col-12">
-            <WaterworksTable />
-          </Card>
-        </div>
+    <div className="container">
+      <div className="row">
+        <Card className="col-12">
+          <WaterworksTable />
+        </Card>
       </div>
-    </>
+    </div>
   )
 }
 
