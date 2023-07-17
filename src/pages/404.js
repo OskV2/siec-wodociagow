bbbc92a2eef6b77ae1201363c05eb8e138b04b89
Maciej Buszkiewicz <maciek.buszkiewicz@ideeconcept.pl>
2023-07-17 19:03:19 +0200

    feat: added 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
 
 const ErrorPage = () => {
   return (
     <>
       <Navbar />
       <main>
-        <h1>Error occured</h1>
-        <p>Could not find this page!</p>
+        <div className="container">
+          <div className="row">
+            <div className="col-12 d-flex justify-content-center align-items-center flex-column">
+              <h1>Nie znaleziono strony</h1>
+              <p>Powrót na <Link className="footer__link" to='/'>Stronę główną</Link></p>
+            </div>
+          </div>
+        </div>
       </main>
+      <Footer />
     </>
   );
 };
