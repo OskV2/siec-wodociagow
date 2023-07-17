ee6a160798a01ccd6d12187a006c1f1c87431cb5
Maciej Buszkiewicz <maciek.buszkiewicz@ideeconcept.pl>
2023-07-17 05:02:04 +0200

    feat: added responsive navabr

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,22 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from 'react-router-dom'
 import './Navbar.scss'
 
+import Close from "../../img/close.svg";
+import Burger from "../../img/burger.svg";
+
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const navLinks = (
+    <div className={'nav__items ' + (isOpen ? 'nav__items--active' : 'nav__items--inactive ')}>
+      <NavLink className={({isActive}) => isActive ? 'nav__item nav__item--active' : 'nav__item'} to='/'>Dashboard</NavLink>
+      <NavLink className={({isActive}) => isActive ? 'nav__item nav__item--active' : 'nav__item'} to='/graph'>Graf</NavLink>
+      <NavLink className={({isActive}) => isActive ? 'nav__item nav__item--active' : 'nav__item'} to='/chart'>Wykres</NavLink>
+      <NavLink className={({isActive}) => isActive ? 'nav__item nav__item--active' : 'nav__item'} to='/table'>Tabela</NavLink>
+    </div>
+  )
+
   return (
     <header>
-      <nav>
+      <nav className="d-none d-lg-block">
         <div className="container">
           <div className="row">
             <div className="nav col-12">
-                <NavLink className={({isActive}) => isActive ? 'nav__item nav__item--active' : 'nav__item'} to='/'>Dashboard</NavLink>
-                <NavLink className={({isActive}) => isActive ? 'nav__item nav__item--active' : 'nav__item'} to='/graph'>Graf</NavLink>
-                <NavLink className={({isActive}) => isActive ? 'nav__item nav__item--active' : 'nav__item'} to='/chart'>Wykres</NavLink>
-                <NavLink className={({isActive}) => isActive ? 'nav__item nav__item--active' : 'nav__item'} to='/table'>Tabela</NavLink>
+              {navLinks}
             </div>
           </div>
         </div>
       </nav>
+
+      <nav className="d-block d-lg-none">
+        <div className="container">
+          <div className="row">
+            <div className="nav col-12">
+              <img className="nav__button" src={isOpen ? Close : Burger} onClick={() => setIsOpen(!isOpen)} />
+              {navLinks}
+            </div>
+          </div>
+        </div>
+      </nav>
+
     </header>
   );
 };
